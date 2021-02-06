19782f499067f12fedf0edfd73fb3397c1aa03bf
Kristin Warren <business510factor@gmail.com>
2021-02-06 12:53:00 -0800

    carousel modal

--- JS/script.js
+++ JS/script.js
@@ -1,24 +1,48 @@
+$(function () {
 
-$(function() {
-    $(".carousel").carousel( { interval: 2000 } );
-    $("#carouselButton").click(function(){
+    // speed of the carousel in milliseconds
+    $(".carousel").carousel({ interval: 500 });
+
+    // when the play or pause button is clicked
+    $("#carouselButton").click(function () {
+
+        // find the <i> tag inside of carouselButton element
+        // does it have the class "fa-pause"
         if ($("#carouselButton").children("i").hasClass("fa-pause")) {
+
+            // stop the carousel
             $(".carousel").carousel("pause");
+
+            // find the <i> tag inside of the carouselButton element
+            // remove the class "fa-pause"
             $("#carouselButton").children("i").removeClass("fa-pause");
+
+            // find the <i> tag inside of the carouselButton element
+            // add the class "fa-play"
             $("#carouselButton").children("i").addClass("fa-play");
+
+
         } else {
+
+            // starts the carousel
             $(".carousel").carousel("cycle");
+
+
+            // find the <i> tag inside of the carouselButton element
+            // removes the class "fa-play"
             $("#carouselButton").children("i").removeClass("fa-play");
-            $("#carouselButton").children("i").addClass("fa-pause"); 
+
+
+            // find the <i> tag inside of the carouselButton element
+            // add the class "fa-pause"
+            $("#carouselButton").children("i").addClass("fa-pause");
         }
     });
 
-    $("#reserveButton").click(function() {
-        $("#reserveModal").modal("show");
-    });
-
-    $("#loginButton").click(function() {
-        $("#loginModal").modal("toggle");
-    });
-
+ $("#reserveButton").click(function() {
+    $("#reserveModal").modal("show");
+ });
+ $("#loginButton").click(function() {
+     $("#loginModal").modal("show");
+});    
 });
